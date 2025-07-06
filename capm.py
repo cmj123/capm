8badb2771a1299760874921ecd2ceb46c401e6d6
Esuabom Dijemeni <e.dijemeni@gmail.com>
2025-07-06 16:53:32 +0100

    Completed CAPM Model

--- capm.py
+++ capm.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import yfinance as yf 
 import matplotlib.pyplot as plt
-
+from datetime import date, timedelta
 # market interest rate
 RISK_FREE_RATE = 0.05 
 # We will consider monthly returns and we want to calculate the annual return
@@ -34,7 +34,7 @@ class CAPM:
         self.data[['s_returns', 'm_returns']] = np.log(self.data[['s_adjclose', 'm_adjclose']] / self.data[['s_adjclose', 'm_adjclose']].shift(1))
 
         # remove the NaN values
-        self.data = self.data[1:]
+        self.data = self.data.dropna()
 
     def calculate_beta(self):
         # covariance matrix: the diagonal items are the variances
@@ -43,9 +43,34 @@ class CAPM:
         covariance_matrix = np.cov(self.data["s_returns"], self.data["m_returns"])
         # calculating beta accroding to the formula
         beta = covariance_matrix[0,1] / covariance_matrix[1,1]
-        print(f'beta')
+        print(f'\nBeta from formula {beta}')
+
+
+    def regression(self):
+        # using linear regression to fit a line to the data
+        # [stock_retirns, market_returns] - slope is the beta
+        beta, alpha = np.polyfit(self.data['m_returns'], self.data['s_returns'], deg=1)
+
+        # Calculate the expected return
+        expected_return = RISK_FREE_RATE + beta * (self.data['m_returns'].mean()*MONTHS_IN_YEAR - RISK_FREE_RATE)
+
+        # Statement of expected return and beta
+        er_beta_str = "Expected Return - " + str(np.round(expected_return*100, 2)) + '%. The beta - ' + str(np.round(beta,2))
+        
+        # Plot figure
+        fig, axis = plt.subplots(1, figsize=(10,6))
+        axis.scatter(self.data["m_returns"], self.data['s_returns'], label="Data Points")
+        axis.plot(self.data["m_returns"], beta*self.data["m_returns"] + alpha, color='red', label="CAPM Line")
+        plt.title('Capital Asset Pricing Model, finding alpha and beta')
+        plt.xlabel('Market return $R_m$')
+        plt.ylabel('Stock return $R_a$')
+        plt.text(0.8, 0.15, er_beta_str , horizontalalignment='right', verticalalignment='top', transform=axis.transAxes)
+        plt.legend()
+        plt.grid(True)
+        plt.show()    
 
 if __name__ == '__main__':
-    capm = CAPM(['IBM', '^GSPC'], '2016-01-01', '2017-01-01')
+    capm = CAPM(['0P0001KCAG.L', '^GSPC'], date.today()- timedelta(days=6*365), date.today())
     capm.initialize()
-    capm.calculate_beta()
+    # capm.calculate_beta() #Interested in beta calculation using covariance matrix
+    capm.regression()
